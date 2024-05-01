9ac23d4c4551d2d688f77bcceafbf04e94f7dd22
Teddy <teddykipchumba17@gmail.com>
2024-05-01 18:56:28 +0300

    Some deliverables completed

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,36 @@
+import { useState,useEffect } from "react";
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({Categories,setDisplay,display}) {
+  const [selectedCategory, setSelectedCategory]=useState("All")
+  useEffect(()=>{
+    if(selectedCategory === "All"){
+      console.log(selectedCategory)
+    }else {
+      console.log(selectedCategory)
+      setDisplay(display.filter((item)=>item.category ===  selectedCategory))
+    }
+  },[selectedCategory,display,setDisplay])
+  function handleClick (category){
+  setSelectedCategory(category)
+  }  
+  function pickCategory (){
+    return(
+    Categories.map((category,index)=>(
+      <button 
+      key={index} 
+      onClick={()=>handleClick(category)}
+      className={selectedCategory === category ?"selected":null}
+      >
+      {category}
+      </button>
+      )))
+  }
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {pickCategory()}
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function Task() {
+function Task({category,text,index,display,setDisplay}) {
+ // const [delId,setdel]=useState("")
+  function handleDelete(){
+    console.log(text)
+    setDisplay(display.filter((item)=>(item.text.trim()!==text.trim())))  
+  }
   return (
-    <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+    <div className="task" id={index}>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={handleDelete}>X</button>
     </div>
   );
 }
